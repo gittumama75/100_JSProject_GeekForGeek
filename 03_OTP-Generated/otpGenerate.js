945c0e670a1adc9dd79de5c28500ef3c83b70674
gittumama75 <hedxenkurigram@gmail.com>
2025-10-18 16:32:52 +0600

    Javascript is done

--- 03_OTP-Generated/otpGenerate.js
+++ 03_OTP-Generated/otpGenerate.js
@@ -1,21 +1,7 @@
 let generatedOTP;
+const otpExpiresElem = document.getElementById("otp-expires-id");
 
-//  Basic Function for the initialization
-function init() {
-    // console.log("Javascript initialization done!!");
-    tackleOTPBoxes();
-    setTimeout(generateOTP, 1000);
-}
-init();
-
-//  Function for the generated OTP
-function generateOTP() {
-    generatedOTP = Math.floor(1000 + Math.random() * 9000);
-    document.getElementById("generated-otp-id").innerText = `Your OTP is : ${generatedOTP}`;
-    console.log(generatedOTP);
-}
-
-//  Function for the tackle OTP Boxes
+//  Function for the tackle OTP boxes
 function tackleOTPBoxes() {
     document.getElementById("otp-box-list-id").addEventListener("input", (e) => {
         if (isNaN(e.target.value)) {
@@ -25,6 +11,60 @@ function tackleOTPBoxes() {
         if (e.target.nextElementSibling) {
             e.target.nextElementSibling.focus();
         }
+        validateOTP();
     });
-    validateOTP();
 }
+
+//  Function for the generate OTP
+function generateOTP() {
+    generatedOTP = Math.floor(1000 + Math.random() * 9000);
+    document.getElementById("generated-otp-id").innerText = `Your OTP is : ${generatedOTP}`;
+    expireOTP();
+}
+
+//  Function for the OTP validation
+function validateOTP() {
+    let typedNumber = "";
+    const otpBoxes = document.getElementById("otp-box-list-id").children;
+    [...otpBoxes].forEach((element) => {
+        typedNumber = typedNumber + element.value;
+
+        const result = generatedOTP === parseInt(typedNumber, 10);
+        const resultElem = document.getElementById("result-id");
+
+        if (result) {
+            document.getElementById("result-id").innerText = `OTP has been validate successfully`;
+            resultElem.classList.remove("fail");
+            resultElem.classList.add("success");
+        } else {
+            document.getElementById("result-id").innerText = `OTP has not valid`;
+            resultElem.classList.remove("success");
+            resultElem.classList.add("fail");
+        }
+    });
+}
+
+//  Function for the checking OTP expiration
+function expireOTP() {
+    const totalTime = 15000;
+    const interval = 1000;
+    let slice = totalTime / interval;
+
+    const intvId = setInterval(function () {
+        otpExpiresElem.innerText = `OTP will expire in ${slice} seconds.`;
+        slice = slice - 1;
+    }, interval);
+
+    setTimeout(function () {
+        otpExpiresElem.innerText = "OTP Expired";
+        clearInterval(intvId);
+    }, totalTime);
+}
+
+//  Function for the initialization
+function init() {
+    console.log("Javascript initialization done!!");
+    tackleOTPBoxes();
+    setTimeout(generateOTP, 2000);
+}
+init();
